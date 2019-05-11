cc90fc4d5353968e98f64f356db2654b95dadf94
smallbeautygirl <a0970318352@gmail.com>
2019-05-11 17:57:10 +0800

    check connect

--- homework/B043040026_HW3/client/client.c
+++ homework/B043040026_HW3/client/client.c
@@ -4,13 +4,16 @@
 #include <netinet/in.h> //Internet Protocol family, ex: sin_family, sin_port, ..., ect
 #include <sys/socket.h>
 #include <arpa/inet.h> //for inet_addr()
+#include <pthread.h>
 #include <math.h>
 #include <unistd.h> //write(.)
 #define SIZE 512
 
 /*require (1.socket() 2.connect() 3.write())build up a TCP client*/
 int mwrite(int sockfd, const void *buf, size_t len);
-void split(char *input, char split_word[3][64]);
+int mread(int sockfd, void *buf, size_t len);
+int split(char *input, char split_word[10][64]);
+void *msgread(void *fd);//一直收訊息
 
 int main(void){
     struct sockaddr_in srv;
@@ -19,14 +22,16 @@ int main(void){
     int fd = -1;
     int server_port = 0;
     char* server_ip = NULL;
+    
 
     while(1){
         char input[64]={'\0'};//the user's input
-        char split_word[4][64]={'\0'};
+        char split_word[10][64]={'\0'};
+        int countword=0;//count how many words the user enter
 
         printf("$");
         fgets(input,64,stdin);
-        split(input,split_word);
+        countword=split(input,split_word);
 
         if(strcmp(split_word[0],"connect")==0){
             fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); //AF_INET是指用IPv4 , SOCK_STREAM select the TCP protocol
@@ -51,20 +56,26 @@ int main(void){
                 perror("connect error");
                 exit(1);
             }
-            if(write(fd,split_word[3],sizeof(split_word[3])))//傳使用者名稱
-            {
-                break;
-            }
+           
             printf("The server with IP \"%s\" has accepted your connection\n", split_word[1]);
+            mwrite(fd,split_word[3],sizeof(split_word[3]));//傳user name
 
+            pthread_t thread; //client thread
+            pthread_create(&thread, NULL,msgread,&fd);
         }
         else if(strcmp(split_word[0],"chat")==0){
             if(fd == -1){
                 printf("Please connect first\n");
             }
             else{
-                
-                
+                int howmanypeople=countword-2;//要傳訊息給幾個人
+                mwrite(fd,&howmanypeople,sizeof(howmanypeople));//傳要傳訊息給幾個人
+                for(int i=1;i<=howmanypeople;++i){
+                    mwrite(fd,split_word[i],sizeof(split_word[i]));//傳每個要收的人的名字
+                    printf("%s\n",split_word[i]);
+                }
+                mwrite(fd,split_word[countword-1],sizeof(split_word[countword-1]));//傳訊息
+                printf("%s\n",split_word[countword-1]);
             }
         }
         else if (strcmp(split_word[0], "bye") == 0)
@@ -101,9 +112,29 @@ int mwrite(int sockfd, const void *buf, size_t len)
     }
 }
 
-void split(char *input, char split_word[4][64])
+
+int mread(int sockfd, void *buf, size_t len)
+{
+    int nbytes = 0;
+    int size = len;
+
+    while ((nbytes = read(sockfd, buf, len)) > 0)
+    {
+        buf += nbytes;
+        len -= nbytes;
+    }
+    if (nbytes < 0)
+        return -1; /* fail to read */
+    else if (len == 0)
+        return size; /* success to read all data */
+    else if (len != 0)
+        return 0; /* socket is closed, read 0 bytes. */
+}
+
+int split(char *input, char split_word[10][64])
 {
     int i = 0, j = 0, k = 0;
+    int count =1;
     // remove the end of line '\n' charactor
     input[strlen(input) - 1] = '\0';
     while (input[i] != '\0')
@@ -117,7 +148,17 @@ void split(char *input, char split_word[4][64])
         {
             k = 0;
             j++;
+            count++;
         }
         i++;
+    }
+    return count;
+}
+
+void *msgread(void *fd){
+    int socket_fd= *(int *)fd;
+    char *message=NULL;
+    while(1){
+        mread(socket_fd,message,sizeof(message));
     }
 }
